0b439056b0f0c86ee885d9b4b9a401a5e9df16bb
xihua <xihua.chen@intel.com>
2020-06-29 10:04:28 +0800

    Update product.mk

--- groups/dbc/true/product.mk
+++ groups/dbc/true/product.mk
@@ -3,4 +3,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.sys.usb.adbover=dwc
 
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/{{_extra_dir}}/dhcp_eth0.sh:recovery/root/vendor/bin/dhcp_eth0.sh
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/{{_extra_dir}}/dhcp_eth0.sh:root/vendor/bin/dhcp_eth0.sh
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/{{_extra_dir}}/dhcp_eth0.sh:vendor/bin/dhcp_eth0.sh
